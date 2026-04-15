bfbe71a641a84a86e3c0dca4df9e3c3de03a980d
CasTolax <cantuncer32@gmail.com>
2026-04-15 10:21:15 +0300

    refactoring codes, and append mod func

--- advanced_calc/calc_main.c
+++ advanced_calc/calc_main.c
@@ -4,110 +4,79 @@
 
 void exit_procs(void);
 
-struct pointers
-{
-    int *p1; // this pointer for a
-    int *p2; // this pointer for b
-    /*write result adress or process adress*/
-};
-
-
-int main(int a,int b)
+int main(void)
 {   
     atexit(exit_procs);
 
     int input;
-    int done = 0; // while loop
+    int a, b;
 
-    printf(" -- advenced Calculator -- \n");
-    printf("Please select process or exit the calculator!\n");
-    printf("1) addition 2) subtraction 3) multiplication 4) division 5) EXIT ");
-    
-    // ask the user for process
-    scanf("%d",&input);
+    printf(" -- advanced Calculator -- \n");
 
-    while(!done){
+    while(1){
 
-        switch (input)
-    {
-    case 1:
-        
-         printf("a = ?");
-        scanf("%d",&a);
+        printf("\n1) add 2) sub 3) mul 4) div 5) mod 6)  exit\n");
+        scanf("%d",&input);
 
-        printf("b = ?"); 
-        scanf("%d",&b);
-
-        int result_add = addition(a,b);
-        printf("result = %d\n",result_add);
-        printf("escape ctrl+c\n");
-
-        break;
-
-    case 2:
-
-         printf("a = ?");
-        scanf("%d",&a);
-
-        printf("b = ?"); 
-        scanf("%d",&b);    
-
-        int result_sub = subtraction(a,b);
-        printf("result = %d\n",result_sub);
-        printf("escape ctrl+c\n");
-
-        break;
-
-    case 3:
-
-         printf("a = ?");
-        scanf("%d",&a);
-
-        printf("b = ?"); 
-        scanf("%d",&b);    
-
-        int result_multip = multiplication(a,b);
-        printf("result = %d\n",result_multip); //! there is a someting wrong
-        printf("escape ctrl+c\n");
-
-        break;
-    case 4:
-
-         printf("a = ?");
-        scanf("%d",&a);
-
-        printf("b = ?"); 
-        scanf("%d",&b);
-
-        int result_div = division(a,b);
-        
-        if(a == NULL || b == NULL){
-            printf("You must cannot division with zero!\n");
-            break;
-        } 
-        else {
-            printf("result = %d\n",result_div);
-            printf("escape ctrl+c\n");
+        if(input == 6){
+            exit(EXIT_SUCCESS);
         }
 
-        break;
-    case 5:
-       exit(EXIT_SUCCESS);
-        break;
+        printf("a = ");
+        scanf("%d",&a);
 
-    default:
-        break;
+        printf("b = ");
+        scanf("%d",&b);
+
+        switch (input)
+        {
+        case 1: {
+            int result = addition(a,b);
+            printf("result = %d\n",result);
+            break;
+        }
+
+        case 2: {
+            int result = subtraction(a,b);
+            printf("result = %d\n",result);
+            break;
+        }
+
+        case 3: {
+            int result = multiplication(a,b);
+            printf("result = %d\n",result);
+            break;
+        }
+
+        case 4: {
+            if(b == 0){
+                printf("Cannot divide by zero!\n");
+            } else {
+                int result = division(a,b);
+                printf("result = %d\n",result);
+            }
+            break;
+        }
+        case 5: {
+            int m;
+
+            printf("m = ");
+            scanf("%d",&m);
+
+            int result = mod(a,b,m);
+            printf("result = %d",result);
+            
+            break;
+        }
+
+        default:
+            printf("Invalid input\n");
+        }
     }
 
-    }
-    
     return 0;
 }
 
-/*
- when user exit from calculator, this function will be start and
- break the loop and write on screen.
-*/ 
 void exit_procs(void)
 {
     printf("Exit is successfully!\n");
